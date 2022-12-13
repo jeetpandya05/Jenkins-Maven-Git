f9190292be56a448dc041c162d36f93aa3a67010
Jeet Pandya <j.jeetpandya@gmail.com>
2022-12-13 15:52:40 +0530

    New Maven Commit

---
+++ MavenTest/LoginTest.java
@@ -0,0 +1,34 @@
+package MavenTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	
+	@Test
+	public void Login() {
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demo.guru99.com/test/login.html");
+		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("passwd")).sendKeys("abc123");
+		driver.findElement(By.id("SubmitLogin")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+	
+	}		
+		
+	
+}
